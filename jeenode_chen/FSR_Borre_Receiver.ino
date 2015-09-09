e2aa8d883eee02ee79832d9b80af58effdc728c7
Thomas Roeggla (MacMini) <tom@lichtfragmente.com>
2015-09-09 17:34:36 +0200

    added chen's source code for th JeeNode sender and receiver

---
+++ jeenode_chen/FSR_Borre_Receiver.ino
@@ -0,0 +1,75 @@
+/// @dir poller
+/// Polls a number of "pollee" nodes as fast as possible to get data from them.
+// 2011-11-23 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+//
+// Warning: this test will flood the radio band so nothing else gets through!
+//
+// Maximum rates will only be achieved if all nodes 1 .. NUM_NODES are powered
+// up, properly configured, and pre-loaded with the pollee.ino sketch.
+
+#include <JeeLib.h>
+
+const byte NUM_NODES = 2; // poll using node ID from 1 to NUM_NODES 
+
+typedef struct {
+  byte node;
+ byte data_1; 
+ byte data_2; 
+   byte data_3; 
+ byte data_4;
+  
+} Payload;
+
+byte nextId;
+MilliTimer timer;
+
+void setup () {
+  Serial.begin(57600);
+  Serial.println("\n[poller]");
+  rf12_initialize(31, RF12_868MHZ, 80);
+}
+
+void loop () {
+  // switch to next node
+  if (++nextId > NUM_NODES)
+    nextId = 1;
+  // send an empty packet to one specific pollee
+  rf12_sendNow(RF12_HDR_ACK | RF12_HDR_DST | nextId, 0, 0);
+  // wait up to 10 milliseconds for a reply
+  timer.set(10);
+  while (!timer.poll())
+    if (rf12_recvDone() && rf12_crc == 0 && rf12_len == sizeof (Payload)) {
+      // got a good ACK packet, print out its contents
+      const Payload* p = (const Payload*) rf12_data;
+    byte a = p->data_1;
+   int b = map(a,0,255,0,1023);
+    byte c = p->data_2;
+    int d = map(c,0,255,0,1023);
+    byte x = p->data_3;
+    int y = map(x,0,255,0,1023);
+     byte z = p->data_4;
+     int k = map(z,0,255,0,1023);
+     Serial.print((word) p->node);
+    Serial.print("    ");
+   Serial.print(":");
+    Serial.print(" ");
+   Serial.print(b);
+     Serial.print("   ");
+     Serial.print(": "); 
+    Serial.print("    ");
+    Serial.print(":");
+     Serial.print(" ");
+     Serial.print(d);
+       Serial.print("    ");
+      Serial.print(":");
+      Serial.print(" ");
+     Serial.print(y);
+      Serial.print("    ");
+      Serial.print(":");
+      Serial.print(" ");
+     Serial.print(k);
+     Serial.print(" \n  ");
+    //Serial.println(p->time);
+      break;
+    }
+}
---
+++ jeenode_chen/FSR_Borre_Sender.ino
@@ -0,0 +1,52 @@
+/// @dir pollee
+  //This the software we used to upload to the pollee. Each second 5 packets/per second, so that each poller has 5*25*2=250 packets/per second, and this is good for sink node to work.
+/// This can run on several nodes, and get data to the central "poller" node.
+// 2011-11-23 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+//
+// Warning: this test will flood the radio band so nothing else gets through!
+//
+// To prepare for this test, you need to upload RF12demo to each pollee first,
+// and set its node ID, group (77) and band (868 MHz). Node IDs must be in the 
+// range 1 .. NUM_NODES, as defined in poller.ino (all nodes must be present).
+
+#include <JeeLib.h>
+#include <Ports.h>
+#include <RF12.h>
+
+Port ldr_3 (3);//setup Jeenode V6 using Port3 
+Port ldr_4 (4);//setup Jeenode V6 using Port4
+Port ldr_1 (1);//setup Jeenode V6 using Port1 
+Port ldr_2 (2);//setup Jeenode V6 using Port2
+
+typedef struct {
+  byte node;
+ byte data_1; 
+ byte data_2;
+  byte data_3; 
+  byte data_4; 
+} Payload;
+
+Payload payload;
+
+void setup () {
+  Serial.begin(57600);
+  Serial.print("\n[pollee]");
+  // use the node ID previously stored in EEPROM by RF12demo
+  payload.node = rf12_config();
+}
+
+void loop () {
+  // wait for an incoming empty packet for us
+  if (rf12_recvDone() && rf12_crc == 0 && rf12_len == 0 && RF12_WANTS_ACK) 
+  {
+    // invent some data to send back
+    //payload.time = millis();
+   payload.data_1 = ldr_1.anaRead();
+   payload.data_2 = ldr_2.anaRead();
+    payload.data_3 = ldr_3.anaRead();
+   payload.data_4 = ldr_4.anaRead();
+    // start transmission
+    rf12_sendStart(RF12_ACK_REPLY, &payload, sizeof payload);
+    
+  }
+}
