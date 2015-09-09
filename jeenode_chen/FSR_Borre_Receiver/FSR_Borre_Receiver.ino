d4203b71332d4746a6fd4475672b29ff37cbc8c3
Thomas Roeggla (MacMini) <tom@lichtfragmente.com>
2015-09-09 18:02:11 +0200

    some code cleanup and sending the data to the serial port as JSON

--- jeenode_chen/FSR_Borre_Receiver/FSR_Borre_Receiver.ino
+++ jeenode_chen/FSR_Borre_Receiver/FSR_Borre_Receiver.ino
@@ -13,11 +13,11 @@ const byte NUM_NODES = 2; // poll using node ID from 1 to NUM_NODES
 
 typedef struct {
   byte node;
- byte data_1; 
- byte data_2; 
-   byte data_3; 
- byte data_4;
   
+  byte data_1; 
+  byte data_2; 
+  byte data_3;
+  byte data_4; 
 } Payload;
 
 byte nextId;
@@ -25,51 +25,42 @@ MilliTimer timer;
 
 void setup () {
   Serial.begin(57600);
-  Serial.println("\n[poller]");
   rf12_initialize(31, RF12_868MHZ, 80);
 }
 
 void loop () {
   // switch to next node
-  if (++nextId > NUM_NODES)
+  if (++nextId > NUM_NODES) {
     nextId = 1;
+  }
+    
   // send an empty packet to one specific pollee
   rf12_sendNow(RF12_HDR_ACK | RF12_HDR_DST | nextId, 0, 0);
   // wait up to 10 milliseconds for a reply
   timer.set(10);
+  
   while (!timer.poll())
     if (rf12_recvDone() && rf12_crc == 0 && rf12_len == sizeof (Payload)) {
       // got a good ACK packet, print out its contents
       const Payload* p = (const Payload*) rf12_data;
-    byte a = p->data_1;
-   int b = map(a,0,255,0,1023);
-    byte c = p->data_2;
-    int d = map(c,0,255,0,1023);
-    byte x = p->data_3;
-    int y = map(x,0,255,0,1023);
-     byte z = p->data_4;
-     int k = map(z,0,255,0,1023);
-     Serial.print((word) p->node);
-    Serial.print("    ");
-   Serial.print(":");
-    Serial.print(" ");
-   Serial.print(b);
-     Serial.print("   ");
-     Serial.print(": "); 
-    Serial.print("    ");
-    Serial.print(":");
-     Serial.print(" ");
-     Serial.print(d);
-       Serial.print("    ");
-      Serial.print(":");
-      Serial.print(" ");
-     Serial.print(y);
-      Serial.print("    ");
-      Serial.print(":");
-      Serial.print(" ");
-     Serial.print(k);
-     Serial.print(" \n  ");
-    //Serial.println(p->time);
+
+      // remap all the values into a range from 0 to 1000
+      int value1 = map(p->data_1, 0, 255, 0, 1000);
+      int value2 = map(p->data_2, 0, 255, 0, 1000);
+      int value3 = map(p->data_3, 0, 255, 0, 1000);
+      int value4 = map(p->data_4, 0, 255, 0, 1000);
+
+      // calculate base ID
+      int base_id = (((int)p->node) - 1) * 4;
+
+      // send data out over the serial interface encoded as JSON
+      Serial.print("{");
+      Serial.print("\""); Serial.print(base_id + 1); Serial.print("\": "); Serial.print(value1);  Serial.print(", ");
+      Serial.print("\""); Serial.print(base_id + 2); Serial.print("\": "); Serial.print(value2);  Serial.print(", ");
+      Serial.print("\""); Serial.print(base_id + 3); Serial.print("\": "); Serial.print(value3);  Serial.print(", ");
+      Serial.print("\""); Serial.print(base_id + 4); Serial.print("\": "); Serial.print(value4);  Serial.print(" ");
+      Serial.println("}");
+      
       break;
     }
 }
