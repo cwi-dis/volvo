6fcf5e40b808bb8160b2c509c03b25f596bf5000
Jack Jansen <Jack.Jansen@cwi.nl>
2015-09-17 22:30:14 +0200

    Read 9 sensors in matrix configuration

---
+++ jeenode_chen/FSR_Borre_MatrixSender/FSR_Borre_MatrixSender.ino
@@ -0,0 +1,79 @@
+/// @dir pollee
+  //This the software we used to upload to the pollee. Each second 5 packets/per second, so that each poller has 5*25*2=250 packets/per second, and this is good for sink node to work.
+/// This can run on several nodes, and get data to the central "poller" node.
+// 2011-11-23 <jc@wippler.nl> http://opensource.org/licenses/mit-license.php
+//
+// Warning: this test will flood the radio band so nothing else gets through!
+//
+// To prepare for this test, you need to upload RF12demo to each pollee first,
+// and set its node ID, group (77) and band (868 MHz). Node IDs must be in the 
+// range 1 .. NUM_NODES, as defined in poller.ino (all nodes must be present).
+
+#include <JeeLib.h>
+#include <Ports.h>
+#include <RF12.h>
+
+#define MAGIC 42
+#define IFDEBUG if(1)
+
+#define NROW 3
+#define NCOLUMN 3
+
+Port ports[] = {
+  Port(1),
+  Port(2),
+  Port(3),
+  NULL
+};
+
+#define NSENSOR (NROW*NCOLUMN)
+typedef struct {
+  byte magic;
+  byte node;
+  
+  byte data[NSENSOR]; 
+} Payload;
+
+Payload payload;
+
+void setup () {
+  IFDEBUG Serial.begin(57600);
+  // use the node ID previously stored in EEPROM by RF12demo
+  payload.node = rf12_config();
+  payload.magic = MAGIC;
+  for (Port *p=ports; *p; p++) {
+  	// Set the digital pin to output, initizalize to HIGH
+  	p->mode(OUTPUT);
+    p->digiWrite(HIGH);
+    // Set the analog pin to input, disable pullup
+    p->mode2(INPUT);
+    p->digiWrite(LOW);
+  }
+}
+
+void loop () {
+  // wait for an incoming empty packet for us
+  if (rf12_recvDone() && rf12_crc == 0 && rf12_len == 0 && RF12_WANTS_ACK) {
+    // read data from the analog pins and store it into the payload struct
+    IFDEBUG Serial.print("poll ");
+    int i = 0;
+    for (int row=0; row<NROW; row++) {
+      // Enable this row of sensors
+      ports[row].digiWrite(LOW);
+      // Not needed? delay(10);
+      for (int col=0; col < NCOLUMN; col++) {
+      	// It is suggested to discard one analog reading to stabilize
+      	// the ADC
+        uint8_t value = ports[col].anaRead();
+        value = ports[col].anaRead();
+        payload.data[i++] = value;
+        IFDEBUG Serial.print((int)value);
+        IFDEBUG Serial.print(' ');
+      }
+      ports[row].digiWrite(HIGH);
+    }
+    IFDEBUG Serial.println();
+    // start transmission
+    rf12_sendStart(RF12_ACK_REPLY, &payload, sizeof payload);  
+  }
+}
