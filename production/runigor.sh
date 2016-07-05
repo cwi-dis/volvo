83c5217570d3fb3233e3c3a265c3e654e3ccef33
Unknown DIS group member <dis-list@cwi.nl>
2016-07-05 16:43:39 +0200

    Forestall igor running if ~/NO-IGOR exists

--- production/runigor.sh
+++ production/runigor.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 dirname=`dirname $0`
 cd $dirname
+if [ -f $HOME/NO-IGOR ]; then
+	echo "Skip running IGOR because $HOME/NO-IGOR exists"
+	exit 1
+fi
 exec python ../../home/homeServer/homeServer -p 9090
